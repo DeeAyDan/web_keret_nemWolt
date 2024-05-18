10d4fa5048da68c22c74d0982b3530411c9b2989
Daniel Kromek <h266845@stud.u-szeged.hu>
2024-05-18 18:21:39 +0200

    Display images

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../../services/food/food.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit{
 
+  foods: String[] = []
+  constructor(private foodService: FoodService) { }
+
+  ngOnInit(): void {
+    this.foods = this.foodService.getAllFood()
+  }
 }
