a9dd0750d0705d57f2c3dea9d97dfa4bac8628b4
Daniel Kromek <h266845@stud.u-szeged.hu>
2024-05-19 12:04:26 +0200

    Add login and register pages to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { FoodPageComponent } from './pages/food-page/food-page.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 export const routes: Routes = [
     {path: '', component: MainPageComponent},
@@ -9,4 +11,6 @@ export const routes: Routes = [
     {path: 'tag/:tag', component: MainPageComponent},
     {path: 'food/:id', component: FoodPageComponent},
     {path: 'cart', component: CartPageComponent},
+    {path: 'login', component: LoginPageComponent},
+    {path: 'register', component: RegisterPageComponent}
 ];
